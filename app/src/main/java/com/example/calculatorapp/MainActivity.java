bcc1c6c23615beef93c9535d824c88cad951c30e
Manuel2u <edodoo011@st.ug.edu.gh>
2023-06-11 14:12:19 +0000

    finished

--- app/src/main/java/com/example/calculatorapp/MainActivity.java
+++ app/src/main/java/com/example/calculatorapp/MainActivity.java
@@ -30,26 +30,47 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         expression = new StringBuilder();
 
-        assignId(button_c, R.id.button_c);
-        assignId(button_ac, R.id.button_ac);
-        assignId(button_P_or_N, R.id.button_P_or_N);
-        assignId(button_percent, R.id.button_percentage);
-        assignId(button_divide, R.id.button_divide);
-        assignId(button_x, R.id.button_x);
-        assignId(button_subtract, R.id.button_subtract);
-        assignId(button_add, R.id.button_add);
-        assignId(button_equalTo, R.id.button_equal);
-        assignId(button_0, R.id.button_0);
-        assignId(button_1, R.id.button_1);
-        assignId(button_2, R.id.button_2);
-        assignId(button_3, R.id.button_3);
-        assignId(button_4, R.id.button_4);
-        assignId(button_5, R.id.button_5);
-        assignId(button_6, R.id.button_6);
-        assignId(button_7, R.id.button_7);
-        assignId(button_8, R.id.button_8);
-        assignId(button_9, R.id.button_9);
-        assignId(button_dot, R.id.button_dot);
+        // Assign IDs and set OnClickListener
+        button_c = findViewById(R.id.button_c);
+        button_c.setOnClickListener(this);
+        button_ac = findViewById(R.id.button_ac);
+        button_ac.setOnClickListener(this);
+        button_P_or_N = findViewById(R.id.button_P_or_N);
+        button_P_or_N.setOnClickListener(this);
+        button_percent = findViewById(R.id.button_percentage);
+        button_percent.setOnClickListener(this);
+        button_divide = findViewById(R.id.button_divide);
+        button_divide.setOnClickListener(this);
+        button_x = findViewById(R.id.button_x);
+        button_x.setOnClickListener(this);
+        button_subtract = findViewById(R.id.button_subtract);
+        button_subtract.setOnClickListener(this);
+        button_add = findViewById(R.id.button_add);
+        button_add.setOnClickListener(this);
+        button_equalTo = findViewById(R.id.button_equal);
+        button_equalTo.setOnClickListener(this);
+        button_0 = findViewById(R.id.button_0);
+        button_0.setOnClickListener(this);
+        button_1 = findViewById(R.id.button_1);
+        button_1.setOnClickListener(this);
+        button_2 = findViewById(R.id.button_2);
+        button_2.setOnClickListener(this);
+        button_3 = findViewById(R.id.button_3);
+        button_3.setOnClickListener(this);
+        button_4 = findViewById(R.id.button_4);
+        button_4.setOnClickListener(this);
+        button_5 = findViewById(R.id.button_5);
+        button_5.setOnClickListener(this);
+        button_6 = findViewById(R.id.button_6);
+        button_6.setOnClickListener(this);
+        button_7 = findViewById(R.id.button_7);
+        button_7.setOnClickListener(this);
+        button_8 = findViewById(R.id.button_8);
+        button_8.setOnClickListener(this);
+        button_9 = findViewById(R.id.button_9);
+        button_9.setOnClickListener(this);
+        button_dot = findViewById(R.id.button_dot);
+        button_dot.setOnClickListener(this);
     }
 
     void assignId(MaterialButton btn, int id) {
@@ -57,10 +78,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btn.setOnClickListener(this);
     }
 
+    boolean isResultDisplayed = false;
+
+
     @Override
     public void onClick(View v) {
         MaterialButton button = (MaterialButton) v;
         String buttonText = button.getText().toString();
+
+        if (isResultDisplayed) {
+            // Replace the screen with zero and start a new expression
+            expression.setLength(0);
+            isResultDisplayed = false;
+        }
 
         if (v.getId() == R.id.button_0 || v.getId() == R.id.button_1 ||
                 v.getId() == R.id.button_2 || v.getId() == R.id.button_3 ||
@@ -70,10 +100,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 v.getId() == R.id.button_dot) {
             // Append digit or decimal point to the expression
             expression.append(buttonText);
+            updateResultDisplay(expression.toString());
         } else if (v.getId() == R.id.button_c) {
             // Remove the last character from the expression
             if (expression.length() > 0) {
                 expression.deleteCharAt(expression.length() - 1);
+                updateResultDisplay(expression.toString());
             }
             if (expression.length() == 0) {
                 result.setText("0");
@@ -84,36 +116,84 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             expression.setLength(0);
             result.setText("0");
             return;
-        } else if (v.getId() == R.id.button_add || v.getId() == R.id.button_subtract ||
-                v.getId() == R.id.button_divide) {
-            // Append the operator to the expression
-            if (expression.length() > 0) {
-                char lastChar = expression.charAt(expression.length() - 1);
-                if (!isOperator(lastChar)) {
-                    expression.append(" ").append(buttonText).append(" ");
-                }
-            }
-        } else if (v.getId() == R.id.button_x) {
-            // Append the multiplication operator to the expression
-            if (expression.length() > 0) {
-                char lastChar = expression.charAt(expression.length() - 1);
-                if (!isOperator(lastChar)) {
-                    expression.append(" * ");
-                }
-            }
         } else if (v.getId() == R.id.button_equal) {
             // Evaluate the expression
             try {
-                double resultValue = evaluateExpression(expression.toString());
-                expression.setLength(0);
-                expression.append(formatResult(resultValue));
+                if (!endsWithOperator(expression)) {
+                    double resultValue = evaluateExpression(expression.toString());
+                    expression.setLength(0);
+                    expression.append(formatResult(resultValue));
+                    result.setText(expression.toString());
+                    isResultDisplayed = true;
+                }
             } catch (ArithmeticException e) {
                 expression.setLength(0);
                 expression.append("Error");
+                result.setText(expression.toString());
+            }
+        } else if (isOperatorButton(v.getId())) {
+            String[] tokens = expression.toString().split(" ");
+            int numTokens = tokens.length;
+
+            // Check if the last token is an operator
+            if (numTokens > 0 && isOperator(tokens[numTokens - 1])) {
+                // Remove the last token (operator)
+                expression.delete(expression.length() - tokens[numTokens - 1].length() - 1, expression.length());
+            }
+
+            // Append the operator to the expression
+            expression.append(" ").append(buttonText).append(" ");
+
+            // Update the result display
+            updateResultDisplay(expression.toString());
+        }
+    }
+
+    private boolean isOperatorButton(int buttonId) {
+        return buttonId == R.id.button_add ||
+                buttonId == R.id.button_subtract ||
+                buttonId == R.id.button_x ||
+                buttonId == R.id.button_divide;
+    }
+
+
+    // Helper method to check if the expression ends with an operator
+    private boolean endsWithOperator(CharSequence expression) {
+        return expression.length() > 0 && isOperator(expression.charAt(expression.length() - 1));
+    }
+
+    private void updateResultDisplay(String expression) {
+        StringBuilder formattedExpression = new StringBuilder();
+
+        // Split the expression into numeric portions and operators
+        String[] parts = expression.split(" ");
+        int partsLength = parts.length;
+
+        // Format each part individually
+        for (int i = 0; i < partsLength; i++) {
+            String part = parts[i];
+
+            // Skip empty parts
+            if (part.isEmpty()) {
+                continue;
+            }
+
+            // Format the number or operator
+            if (isNumeric(part)) {
+                part = formatNumber(part);
+            } else if (part.equalsIgnoreCase("x")) {
+                part = "*";
+            }
+
+            formattedExpression.append(part);
+
+            // Add a space after each part except the last one
+            if (i < partsLength - 1) {
+                formattedExpression.append(" ");
             }
         }
 
-        result.setText(expression.toString());
+        result.setText(formattedExpression.toString());
     }
 
     private boolean isOperator(char ch) {
@@ -126,51 +206,66 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         return decimalFormat.format(result);
     }
 
+    private String formatNumber(String number) {
+        // Format the number with commas
+        DecimalFormat decimalFormat = new DecimalFormat("#,###");
+        return decimalFormat.format(Double.parseDouble(number));
+    }
+
+    private boolean isNumeric(String str) {
+        // Check if the string is numeric
+        return str.matches("-?\\d+(\\.\\d+)?");
+    }
+
+
 
     private double evaluateExpression(String expression) {
         String[] tokens = expression.split(" ");
         List<Double> operands = new ArrayList<>();
         List<String> operators = new ArrayList<>();
 
+        // Remove any empty tokens caused by consecutive operators
         for (String token : tokens) {
-            if (isOperator(token)) {
-                operators.add(token);
-            } else {
-                try {
-                    double operand = Double.parseDouble(token);
-                    operands.add(operand);
-                } catch (NumberFormatException e) {
-                    // Invalid operand
-                    return 0;
+            if (!token.isEmpty()) {
+                if (isOperator(token)) {
+                    operators.add(token);
+                } else {
+                    try {
+                        double operand = Double.parseDouble(token);
+                        operands.add(operand);
+                    } catch (NumberFormatException e) {
+                        // Invalid operand
+                        return 0;
+                    }
                 }
             }
         }
 
-        if (operands.size() != operators.size() + 1) {
-            // Invalid expression
+        if (operands.size() == 0) {
+            // No operands found
             return 0;
         }
 
-        for (int i = 0; i < operators.size(); i++) {
-            String operator = operators.get(i);
-            double operand1 = operands.get(i);
-            double operand2 = operands.get(i + 1);
-            double result;
+        double result = operands.get(0);
+        int operatorIndex = 0;
+
+        for (int i = 1; i < operands.size(); i++) {
+            String operator = operators.get(operatorIndex);
+            double operand = operands.get(i);
 
             switch (operator) {
                 case "+":
-                    result = operand1 + operand2;
+                    result += operand;
                     break;
                 case "-":
-                    result = operand1 - operand2;
+                    result -= operand;
                     break;
-                case "X":
                 case "*":
-                    result = operand1 * operand2;
+                    result *= operand;
                     break;
                 case "/":
-                    if (operand2 != 0) {
-                        result = operand1 / operand2;
+                    if (operand != 0) {
+                        result /= operand;
                     } else {
                         // Division by zero error
                         return 0;
@@ -181,13 +276,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     return 0;
             }
 
-            operands.set(i + 1, result);
+            operatorIndex++;
         }
 
-        return operands.get(operands.size() - 1);
+        return result;
     }
 
+
+
     private boolean isOperator(String token) {
-        return token.equals("+") || token.equals("-") || token.equals("X") || token.equals("/") || token.equals("*");
+        return token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/");
     }
 }
